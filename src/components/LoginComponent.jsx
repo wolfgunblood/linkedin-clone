c8f4f5bfa136fea7879cb606479890473561875d
wolfgunblood <jalajdorai1@gmail.com>
2023-05-13 23:02:25 +0530

    Added assets

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
+import React,{ useState } from 'react'
+import LinkedinLogo from '../assets/linkedinLogo.png';
 const LoginComponent = () => {
   return (
-    <div>LoginComponent</div>
+    <div className='login-wrapper'>
+      <img src="" alt="" />
+    </div>
   )
 }
 
