1e299a753709ec749befceaf7099d0e3be387187
wolfgunblood <jalajdorai1@gmail.com>
2023-05-13 23:14:44 +0530

    Added styles to Login Component

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,9 +1,15 @@
 import React,{ useState } from 'react'
 import LinkedinLogo from '../assets/linkedinLogo.png';
+import '../Sass/LoginComponent.scss';
 const LoginComponent = () => {
   return (
     <div className='login-wrapper'>
-      <img src="" alt="" />
+      <img src={LinkedinLogo} alt="Linkedin Logo" className='linkedin-logo' />
+
+      <div className='login-form'>
+        <h1 className='heading'>Sign In</h1>
+        <p className='sub-heading'>Stay Updated on your professional world</p>
+      </div>
     </div>
   )
 }
