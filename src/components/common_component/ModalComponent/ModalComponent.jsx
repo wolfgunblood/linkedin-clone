63372c8551ec15c7253dc2cfe1739c2356a99cae
wolfgunblood <jalajdorai1@gmail.com>
2023-05-14 20:21:43 +0530

    Updated modal Component

--- src/components/common_component/ModalComponent/ModalComponent.jsx
+++ src/components/common_component/ModalComponent/ModalComponent.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import "./ModalComponent.scss";
 
-const ModalComponent = () => {
+const ModalComponent = ({
+  status,
+  setStatus,
+  showModal,
+  openModal,
+  closeModal,
+}) => {
   return (
-    <div>ModalComponent</div>
+    <>
+      <Modal
+        title="Create a Post"
+        centered
+        open={showModal}
+        onOk={() => {
+          openModal();
+        }}
+        onCancel={() => {
+          closeModal();
+        }}
+        footer={[
+          <Button
+            key="submit"
+            type='primary'
+            onClick={() => { }}
+            disabled={status.length > 0 ? false : true}
+          >
+            Post
+          </Button>,
+        ]}
+      >
+        <div className='posts-body'>
+          <ReactQuill
+            className='modal-input'
+            theme='snow'
+            placeholder='Write something....'
+            value={status}
+            onChange={(e) => setStatus(e)}
+          />
+        </div>
+      </Modal>
+    </>
   )
 }
 
--- src/components/common_component/PostStatus/PostStatus.jsx
+++ src/components/common_component/PostStatus/PostStatus.jsx
@@ -3,16 +3,34 @@ import './PostStatus.scss';
 import ModalComponent from '../ModalComponent/ModalComponent';
 
 const PostStatus = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [status, setStatus] = useState('');
+  const openModal = () => {
+    setShowModal(true);
+  }
+  const closeModal = () => {
+    setShowModal(false);
+  }
+  // const ModalComponentMemo = useMemo(() => <ModalComponent showModal={showModal} closeModal={closeModal} />,[showModal]);
+  // console.log(ModalComponentMemo);
   return (
     <div className='post-status-container'>
       <div className='user-details'>
         <button
           className='open-post-modal'
+          onClick={openModal}
         >
           Start a Post
         </button>
       </div>
-      <ModalComponent />
+      <ModalComponent 
+        showModal={showModal}
+        setShowModal={setShowModal}
+        closeModal={closeModal}
+        openModal={openModal}
+        status={status}
+        setStatus={setStatus}
+      />
     </div>
   )
 }
