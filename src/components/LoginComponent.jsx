8c7ae576981449ed2017587bef884d780bed0c56
wolfgunblood <jalajdorai1@gmail.com>
2023-05-14 09:05:01 +0530

    Added google Auth2.0

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,7 +1,12 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
 import LinkedinLogo from '../assets/linkedinLogo.png';
 import '../Sass/LoginComponent.scss';
+import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+
 const LoginComponent = () => {
+  let navigate = useNavigate();
+  const [cred, setCred] = useState({});
   return (
     <div className='login-wrapper'>
       <img src={LinkedinLogo} alt="Linkedin Logo" className='linkedin-logo' />
@@ -11,26 +16,44 @@ const LoginComponent = () => {
         <p className='sub-heading'>Stay Updated on your professional world</p>
 
         <div className='auth-inputs'>
-          <input 
-            type="text" 
+          <input
+            type="text"
             placeholder='Email or Phone'
             className='common-input'
-            onChange={(e) => {}}
-            />
-          <input 
+            onChange={(e) => { }}
+          />
+          <input
             type="password"
             placeholder='Password'
             className='common-input'
-            onChange={(e) => {}}
+            onChange={(e) => { }}
           />
         </div>
-        <button onClick={() => {}} className='login-btn' >
+        <button onClick={() => { }} className='login-btn' >
           Sign IN
         </button>
-        <hr className="hr-text" data-content="or" />
-        
+      </div>
+      <hr className="hr-text" data-content="or" />
+      <div className='google-login-container'>
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+          shape='pill'
+          className='google-login-btn'
+        />;
+        <p className='google-login-text'>
+          New to LinkedIn?{" "}
+          <span className='sign-up-link'>
+            Join Now
+          </span>
+        </p>
       </div>
     </div>
+
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,15 @@ import './index.scss';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes';
 import { app, auth } from './firebaseConfig';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <GoogleOAuthProvider clientId={clientId}>
+      <RouterProvider router={router} />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
