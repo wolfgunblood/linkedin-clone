260594278e90bb8fbccde2255a15fe382615000c
wolfgunblood <jalajdorai1@gmail.com>
2023-05-14 19:01:29 +0530

    Updated TopBar Component

--- src/components/common_component/TopBar/TopBar.jsx
+++ src/components/common_component/TopBar/TopBar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import "./TopBar.scss";
+import LinkedInlogo from '../../../assets/linkedinLogo.png';
+import UserLogo from '../../../assets/user.png';
+import {
+    AiOutlineSearch,
+    AiOutlineUserSwitch
+} from 'react-icons/ai';
+import { BiHome,BiMessageDots } from 'react-icons/bi';
+import { FiBriefcase } from 'react-icons/fi';
+import { HiOutlineBell } from 'react-icons/hi';
 
 const TopBar = () => {
-  return (
-    <div>TopBar</div>
-  )
+    return (
+        <div className='topbar-main'>
+            <img src={LinkedInlogo} alt="Linkedin logo" className='linkedin-logo' />
+
+            <div className='react-icons'>
+                <AiOutlineSearch
+                    size={30}
+                    className='react-icon'
+                    onClick={() => { }}
+                />
+                <BiHome
+                    size={30}
+                    className='react-icon'
+                    onClick={() => { }}
+                />
+                <AiOutlineUserSwitch
+                    size={30}
+                    className='react-icon'
+                    onClick={() => { }}
+                />
+                <FiBriefcase size={30} className='react-icon' />
+                <BiMessageDots size={30} className='react-icon' />
+                <HiOutlineBell size={30} className='react-icon' />
+
+            </div>
+            <img src={UserLogo} alt="user-logo" className='user-logo' />
+        </div>
+    )
 }
 
 export default TopBar
