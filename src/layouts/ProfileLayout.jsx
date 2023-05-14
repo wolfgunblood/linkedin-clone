d3ba93b8adf09ea4ed134015baaac0044212d5d1
wolfgunblood <jalajdorai1@gmail.com>
2023-05-14 21:26:41 +0530

    Added Profile Component

---
+++ src/layouts/ProfileLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopBar from '../components/common_component/TopBar/TopBar'
+import Profile from '../Pages/Profile'
+
+const ProfileLayout = () => {
+  return (
+    <>
+    <TopBar />
+    <Profile />
+    </>
+  )
+}
+
+export default ProfileLayout
