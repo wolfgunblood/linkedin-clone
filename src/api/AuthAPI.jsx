ae44cb09be05d7162c00c8900105eede22e31ef5
wolfgunblood <jalajdorai1@gmail.com>
2023-05-14 11:28:47 +0530

    Added API for login sign up and log out

--- src/api/AuthAPI.jsx
+++ src/api/AuthAPI.jsx
@@ -1,14 +1,49 @@
 
 import { auth } from '../firebaseConfig';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import {
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+} from 'firebase/auth';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export const LoginAPI = (email,phone) => {
-    try{
-        let response = signInWithEmailAndPassword(auth,email,phone);
+export const LoginAPI = (email, phone) => {
+    try {
+        let response = signInWithEmailAndPassword(auth, email, phone);
         return response;
+    } catch (error) {
+        return error;
+    }
+};
+
+export const SignUpAPI = (email, phone) => {
+    try{
+        let response = createUserWithEmailAndPassword(auth, email, phone);
+        return response;
+    
     }catch(error){
         return error;
     }
 };
+
+export const GoogleLoginAPI = () => {
+    try {
+        let response = signInWithPopup(auth, new GoogleAuthProvider());
+        return response;
+    } catch (error) {
+        return error;
+    }
+
+};
+
+export const LogoutAPI = () => {
+    try {
+        let response = signOut(auth);
+        return response;
+    } catch (error) {
+        return error;
+    }
+}
